d58b7dc438ca280291a302f04622689c6bb84305
Deepak <sah.sslpu@gmail.com>
2020-12-30 21:59:52 +0530

    Add a bare bones rpc structure

--- src/master/master.go
+++ src/master/master.go
@@ -1,6 +1,25 @@
 package master
 
+import "net/rpc"
+
 // Master defines a master process.
 type Master struct {
+	Files []string
+}
 
+// Args defines a type for RPC exchange.
+type Args struct {}
+
+// Reply defines a type for RPC exchange.
+type Reply struct {}
+
+// StartServer starts a gRPC server.
+func (m *Master) StartServer() {
+	rpc.Register(m)
+	rpc.HandleHTTP()
+}
+
+// GetWork assigns work to worker nodes.
+func (m *Master) GetWork(args *Args, reply *Reply) error {
+	return nil
 }
