5bd42f5f4dc6dc27150deaa8e2c64e13728ec6f6
Deepak <sah.sslpu@gmail.com>
2021-01-01 23:45:55 +0530

    Add plugin loader to worker

--- src/worker/worker.go
+++ src/worker/worker.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"net/rpc"
+	"os"
+	"plugin"
 
 	"github.com/goku321/dist-map-reduce/src/model"
 	log "github.com/sirupsen/logrus"
@@ -9,6 +12,12 @@ import (
 
 // Worker defines a worker process.
 type Worker struct {
+}
+
+// KeyValue represents a key-value pair.
+type KeyValue struct {
+	Key   string
+	Value string
 }
 
 // New creates a new instance of Worker.
@@ -35,6 +44,31 @@ func (w *Worker) Start() {
 }
 
 func main() {
+	log.SetFormatter(&log.JSONFormatter{})
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: worker app.so\n")
+		os.Exit(1)
+	}
 	w := New()
 	w.Start()
+}
+
+// Load the application map and reduce function from a plugin file.
+func loadPlugin(filename string) (func(string, string) []KeyValue, func(string, []string) string) {
+	p, err := plugin.Open(filename)
+	if err != nil {
+		log.Fatalf("cannot load plugin %v", filename)
+	}
+	xmapf, err := p.Lookup("Map")
+	if err != nil {
+		log.Fatalf("cannot find Map in %v", filename)
+	}
+	mapf := xmapf.(func(string, string) []KeyValue)
+	xreducef, err := p.Lookup("Reduce")
+	if err != nil {
+		log.Fatalf("cannot find Reduce in %v", filename)
+	}
+	reducef := xreducef.(func(string, []string) string)
+
+	return mapf, reducef
 }
