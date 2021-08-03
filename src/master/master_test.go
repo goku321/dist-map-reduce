381feb9b38d9ffb7a65a91628108aa45891e0d8c
Deepak <sah.sslpu@gmail.com>
2021-08-03 23:57:53 +0530

    Add test for getPendingMapTask

--- src/master/master_test.go
+++ src/master/master_test.go
@@ -6,7 +6,13 @@ import (
 	"testing"
 	"time"
 
+	"github.com/goku321/dist-map-reduce/src/model"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+)
+
+var (
+	mockFiles = []string{"file1", "file2", "file3"}
 )
 
 func TestGoroutinesCount(t *testing.T) {
@@ -15,4 +21,17 @@ func TestGoroutinesCount(t *testing.T) {
 	// Wait for goroutines to start.
 	time.Sleep(1 * time.Second)
 	require.Equal(t, 8, runtime.NumGoroutine())
+}
+
+func TestGetPendingMapTask(t *testing.T) {
+	mockMaster := New(mockFiles, 4)
+	task := mockMaster.getPendingMapTask()
+
+	t.Run("task should not be nil", func(t *testing.T) {
+		assert.NotNil(t, task)
+	})
+
+	t.Run("task should of map type", func(t *testing.T) {
+		assert.Equal(t, model.Map, task.Type)
+	})
 }
